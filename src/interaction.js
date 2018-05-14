870c14d4ecee3e50ea9e5488ca8135d9fca66c11
Jam Risser <jam@jamrizzi.com>
2018-05-14 19:22:19 +0200

    Added scroll and scripts

--- src/interaction.js
+++ src/interaction.js
@@ -1,5 +1,8 @@
+import _ from 'lodash';
+import fs from 'fs-extra';
 import joi from 'joi';
 import joiValidate from 'easy-joi';
+import path from 'path';
 import { FINISHED, READY, WORKING } from './constants';
 import { evaluate } from './browser';
 
@@ -13,6 +16,8 @@ export default class Interaction {
       elements = [],
       fields = {},
       keys,
+      scripts,
+      scroll,
       timeout
     } = this.config;
     this.click = click;
@@ -22,15 +27,45 @@ export default class Interaction {
     this.keys = keys;
     this.name = name;
     this.url = url;
+    this.scripts = scripts;
+    this.scroll = scroll;
     this.timeout =
       timeout !== true && Number(timeout) > 0 ? Number(timeout) : 1000;
     this.url = url;
     this.waitUntil = timeout ? 'networkidle' : 'load';
   }
 
-  loadConfig({ click, delay, elements, fields, key, keys = [], timeout }) {
+  loadConfig({
+    click,
+    delay,
+    elements,
+    fields,
+    key,
+    keys = [],
+    script,
+    scripts = [],
+    scroll,
+    timeout
+  }) {
     if (key) keys.push(key);
-    return { click, delay, elements, fields, keys, timeout };
+    if (script) scripts.push(script);
+    scripts = _.map(scripts, script => {
+      if (/[\w\s_\-.\/\\]+/g.text(script)) {
+        try {
+          return fs.readFileSync(path.resolve(script), 'utf8');
+        } catch (err) {
+          return '';
+        }
+      }
+      return script;
+    });
+    if (_.isNumber(scroll)) {
+      scroll = {
+        count: scroll,
+        timeout: 10000
+      };
+    }
+    return { click, delay, elements, fields, keys, scripts, scroll, timeout };
   }
 
   async init() {
@@ -50,6 +85,17 @@ export default class Interaction {
         .array()
         .items(joi.string())
         .optional(),
+      scripts: joi
+        .array()
+        .items(joi.string())
+        .optional(),
+      scroll: joi
+        .object()
+        .keys({
+          count: joi.number(),
+          timeout: joi.number()
+        })
+        .optional(),
       timeout: joi.number().optional()
     });
     return true;
@@ -64,7 +110,8 @@ export default class Interaction {
     const { page } = await evaluate(this.url, runInteraction, {
       click: this.click,
       elements: this.elements,
-      fields: this.fields
+      fields: this.fields,
+      scripts: this.scripts
     });
     if (!this.click && !this.keys && this.delay) {
       await new Promise(r => setTimeout(r, this.delay));
@@ -97,7 +144,7 @@ export default class Interaction {
 }
 
 /* eslint-disable prefer-arrow-callback,func-names,no-var,no-undef */
-async function runInteraction({ click, fields, elements }) {
+async function runInteraction({ click, elements, fields, scripts, scroll }) {
   function mergeElement(element, config) {
     Object.keys(config).forEach(function(key) {
       var configValue = config[key];
@@ -111,44 +158,76 @@ async function runInteraction({ click, fields, elements }) {
       }
     });
   }
-  Object.keys(fields).forEach(function(key) {
-    var field = fields[key];
-    document.getElementsByName(key).forEach(function(element) {
-      element.focus();
-      if (element.type === 'checkbox') {
-        element.checked = !!field;
-      } else if (typeof field === 'string') {
-        element.value = field;
-      } else {
-        mergeElement(element, field);
-      }
-      element.dispatchEvent(new Event('change'));
-      element.blur();
+  function scrollToBottom(count, timeout) {
+    count--;
+    return new Promise(resolve => {
+      window.setTimeout(() => {
+        window.scrollTo(0, document.body.scrollHeight);
+        if (count > 0) return resolve(scroll(count, timeout));
+        return resolve();
+      }, timeout);
     });
-  });
-  elements.forEach(elementConfig => {
-    document
-      .querySelectorAll(elementConfig.selector)
-      .forEach(function(element) {
-        if (elementConfig.field) element.focus();
-        mergeElement(element, elementConfig.value);
-        if (elementConfig.field) {
+  }
+  return Promise.resolve()
+    .then(() => {
+      Object.keys(fields).forEach(function(key) {
+        var field = fields[key];
+        document.getElementsByName(key).forEach(function(element) {
+          element.focus();
+          if (element.type === 'checkbox') {
+            element.checked = !!field;
+          } else if (typeof field === 'string') {
+            element.value = field;
+          } else {
+            mergeElement(element, field);
+          }
           element.dispatchEvent(new Event('change'));
           element.blur();
-        }
-      });
-  });
-  if (click) {
-    if (typeof click === 'string') {
-      document.querySelectorAll(click).forEach(function(element) {
-        element.click();
-      });
-    } else {
-      click.forEach(function(selector) {
-        document.querySelectorAll(selector).forEach(function(element) {
-          element.click();
         });
       });
-    }
-  }
+      return null;
+    })
+    .then(() => {
+      elements.forEach(elementConfig => {
+        document
+          .querySelectorAll(elementConfig.selector)
+          .forEach(function(element) {
+            if (elementConfig.field) element.focus();
+            mergeElement(element, elementConfig.value);
+            if (elementConfig.field) {
+              element.dispatchEvent(new Event('change'));
+              element.blur();
+            }
+          });
+      });
+      return null;
+    })
+    .then(() => {
+      if (click) {
+        if (typeof click === 'string') {
+          document.querySelectorAll(click).forEach(function(element) {
+            element.click();
+          });
+        } else {
+          click.forEach(function(selector) {
+            document.querySelectorAll(selector).forEach(function(element) {
+              element.click();
+            });
+          });
+        }
+      }
+      return null;
+    })
+    .then(() => {
+      if (scroll) return scrollToBottom(scroll.count, scroll.timeout);
+      return null;
+    })
+    .then(() => {
+      if (scripts) {
+        scripts.forEach(function(script) {
+          // eslint-disable-next-line no-eval
+          eval(script);
+        });
+      }
+    });
 }
